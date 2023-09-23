0f61f09df2266026b3e9b737995ee15e3231fe55
IVainqueur <ishimvainqueur@gmail.com>
2023-09-23 19:49:27 +0200

    [fix]

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -5,7 +5,5 @@ const { join } = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  ...(process.env.NODE_ENV === 'production' ? {
     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  }: {})
 };
